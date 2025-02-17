0bbb1dae5fca2eb11bfc64c525444bbd39d50cd0
Paulo Ricardo Malta Leal <prmaltaleal@gmail.com>
2025-02-17 11:25:52 -0300

    feat: make get with QueryRow

--- banco-de-dados/1/main.go
+++ banco-de-dados/1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -27,7 +28,7 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-	product := NewProduct("Caneca", 37.99)
+	product := NewProduct("Fone", 152.0)
 	err = insertProduct(db, product)
 	if err != nil {
 		panic(err)
@@ -37,6 +38,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	p, err := getProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("O produto %s custa %.2f\n", p.Name, p.Price)
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -63,4 +70,18 @@ func updateProduct(db *sql.DB, product *Product) error {
 		return err
 	}
 	return nil
+}
+
+func getProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("SELECT id, name, price FROM products WHERE id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
 }
